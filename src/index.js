73477449d11ab4c65e37e9d414d87f0a7b6f3264
mkeksz <mrkeksz@yandex.ru>
2021-09-13 15:37:23 +0300

    Add popups

--- src/index.js
+++ src/index.js
@@ -8,3 +8,39 @@ window.jQuery = $
 window.$ = $
 
 new WOW().init()
+windowsHandler()
+
+
+function windowsHandler() {
+    const windows = document.body.querySelectorAll('[data-window-id]')
+    const buttonsOpen = document.body.querySelectorAll('[data-open-window]')
+    const buttonsClose = document.body.querySelectorAll('[data-close-window]')
+
+    for (const button of buttonsOpen) {
+        button.addEventListener('click', () => {
+            const targetWindowID = button.getAttribute('data-open-window')
+            const window = getWindowByID(targetWindowID)
+            if (!window) return
+            window.classList.add('show')
+            document.body.style.overflow = 'hidden'
+        })
+    }
+
+    for (const button of buttonsClose) {
+        button.addEventListener('click', () => {
+            const targetWindowID = button.getAttribute('data-close-window')
+            const window = getWindowByID(targetWindowID)
+            if (!window) return
+            window.classList.remove('show')
+            document.body.style.overflow = ''
+        })
+    }
+
+    function getWindowByID(id) {
+        for (const window of windows) {
+            const windowID = window.getAttribute('data-window-id')
+            if (!windowID || windowID !== id) continue
+            return window
+        }
+    }
+}
