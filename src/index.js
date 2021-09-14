fe88c9c3c2016ff34007dfeebb78316d22fc3724
mkeksz <mrkeksz@yandex.ru>
2021-09-14 15:40:27 +0300

    Add animations.sass Improve main-button and main-block and glass-block

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+import 'animate.css'
 import './style/main.sass'
 import $ from 'jquery'
 import 'slick-carousel'
-import 'animate.css'
 import WOW from 'wow.js'
 
 window.jQuery = $
 window.$ = $
 
-new WOW().init()
+new WOW({animateClass: 'animated'}).init()
 windowsHandler()
 
 
